e09d1b7d2bac54ef3a40025884a70b32641ef282
litao <li.tao@klotz.de>
2023-05-07 00:39:26 +0800

    config vuex and Router and Jest for project

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,22 @@
     <div id="app">
         <!-- <Counter2/> -->
         <!-- <book></book> -->
-        <Cup></Cup>
+        <!-- <Cup></Cup> -->
     </div>
 </template>
 
-<script>
 
-// import Counter2 from './components/02-counter/Counter.vue'
+
+<script>
+//  import Counter2 from './components/02-counter/Counter.vue'
 // import book from './components/bookstore/book.vue'
-import Cup from './components/cupsCollections/cup.vue'
-export default ({
-    name:"App",
-    components:{
-        // Counter2,
-        // book,
-        Cup
-    }
-})
+// import Cup from './components/cupsCollections/cup.vue'
+// export default ({
+//     name:"App",
+//     components:{
+//         // Counter2,
+//         book,
+//         // Cup
+//     }
+// })
 </script>
---
+++ src/router/index.js
@@ -0,0 +1,18 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Login from '../views/login/login.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Login',
+    component: Login
+  }
+ 
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
